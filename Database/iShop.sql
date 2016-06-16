372328a5bc3299d01ef7ad3049951983b8f562f5
Dato Chkhatarashvili <dchkh12@freeuni.edu.ge>
2016-06-16 12:37:48 +0400

    Changes In Database

--- Database/iShop.sql
+++ Database/iShop.sql
@@ -30,10 +30,49 @@ create table shop_category(
 create table shop(
 	shop_id int not null auto_increment,
     shop_name varchar(100) not null,
+    shop_email varchar(64) not null unique key,
+    shop_password varchar(64) not null,
+    shop_site varchar (64) not null,
     shop_category int not null,
+    shop_info text,
     constraint shop_pk primary key(shop_id),
     constraint shop_category_fk foreign key (shop_category) references shop_category(category_id)
 
 
+);
+
+create table item_category(
+	item_category_id int not null auto_increment,
+    item_category_name varchar(64) not null unique key,
+    constraint item_category_id_pk  primary key(item_category_id)
+    
+
+);
+
+create table item_type(
+	item_type int not null auto_increment,
+	item_type_name varchar(64) not null,
+    item_category_id int not null,
+    constraint item_type_pk primary key(item_type),
+    constraint item_type_fk foreign key(item_category_id) references item_category(item_category_id)
+
+);
+
+create table item_brand(
+	item_brand int not null auto_increment,
+    item_brand_name varchar(64) not null unique key,
+    constraint item_brand_pk primary key(item_brand)
+
+);
+
+create table item(
+	item_id int not null auto_increment,
+    item_name varchar(64) not null,
+    item_type int not null,
+    item_description text,
+    shop_id int not null,
+    constraint item_id_pk primary key(item_id),
+    constraint item_type_fk foreign key(item_type) references item_type(item_type_id),
+    constraint item_shop_id_fk foreign key(shop_id) references shop_id(shop_id)
 );
 
